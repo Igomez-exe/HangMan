50726441078bd9372575de10227ce03ffe8a43de
Ignacio Gomez <ignacio.gomez2901@gmail.com>
2022-04-10 02:49:09 -0300

    commit inicial

---
+++ tareaUno.py
@@ -0,0 +1,33 @@
+import random
+
+with open("palabras.txt", "r") as datos:
+    valores = []
+    
+    for linea in datos:
+        valores = ((linea.split(",")))
+    
+    print(valores)
+    cantidad_de_palabras = len(valores)
+    posicion = random.randrange(0,cantidad_de_palabras)
+    print(posicion)
+    print(valores[posicion])
+    otros_valores = valores[posicion]
+
+    for x in valores[posicion]:
+        print("x",end = " ")
+    
+    print("")
+    
+    no_acertar = True
+    while no_acertar:
+        letra = input("Inserte Letra:")
+        for x in valores[posicion]:
+            print("x",end = " ")
+            if letra in valores[posicion]:
+                print(otros_valores)
+            
+        no_acertar = False
+        print("si")
+
+
+    
