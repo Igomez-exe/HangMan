234692d494f6dd02746c9a5d3eb8222dd6fb36a8
Ignacio Gomez <ignacio.gomez2901@gmail.com>
2022-04-11 01:55:36 -0300

    minimos cambios

--- tareaUno.py
+++ tareaUno.py
@@ -9,25 +9,32 @@ with open("palabras.txt", "r") as datos:
     print(valores)
     cantidad_de_palabras = len(valores)
     posicion = random.randrange(0,cantidad_de_palabras)
-    print(posicion)
     print(valores[posicion])
     otros_valores = valores[posicion]
 
+    almacenar_x = []
     for x in valores[posicion]:
-        print("x",end = " ")
+        almacenar_x.append("x")
+    for x in almacenar_x:
+        print(x, end = " ")
+
     
     print("")
     
     no_acertar = True
+    posicion_letra = 0
     while no_acertar:
         letra = input("Inserte Letra:")
         for x in valores[posicion]:
-            print("x",end = " ")
-            if letra in valores[posicion]:
-                print(otros_valores)
+            
+            if letra == x:
+                almacenar_x[posicion_letra] = letra
+            
+            posicion_letra = posicion_letra + 1 
+            
             
         no_acertar = False
-        print("sissiisi")
 
 
-    
+    for x in almacenar_x:
+        print(x, end = " ")
